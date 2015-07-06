0db5248f4ab04e009d4df73c005dfae551fdc302
dremedios <dremedios@gmail.com>
2015-07-06 21:18:54 +0100

    WiFiControlActivity. Listagem das redes WiFi (scanNetworks...) Com eventos nas labels das listas para redimensionar...

--- WiFiDirectDemo2/src/com/example/android/wifidirect/WiFiControlActivity.java
+++ WiFiDirectDemo2/src/com/example/android/wifidirect/WiFiControlActivity.java
@@ -12,16 +12,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
 
 /**
  * Created by DR AT on 06/07/2015.
- *
+ * <p/>
  * Note: The broadcast executes continuously, in delay of (???) 15 secs, we stopped it
  * when received the first scan
- *
  */
 public class WiFiControlActivity extends Activity {
 
@@ -36,6 +36,8 @@ public class WiFiControlActivity extends Activity {
     ExpandableListView expListViewScannedNetworks;
     ExpandableListAdapter<String, String> expListViewAdapterScannedNetworks;
     IntentFilter intentFilter;
+    ExpandableListView expListViewConfiguredNetworks;
+    ExpandableListAdapter<String, String> expListViewAdapterConfiguredNetworks;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -54,21 +56,64 @@ public class WiFiControlActivity extends Activity {
         expListViewAdapterScannedNetworks = new ExpandableListAdapter<>(this);
         expListViewScannedNetworks.setAdapter(expListViewAdapterScannedNetworks);
 
+        expListViewConfiguredNetworks = (ExpandableListView) findViewById(R.id.expListViewConfiguredNetworks);
+        expListViewAdapterConfiguredNetworks = new ExpandableListAdapter<>(this);
+        expListViewConfiguredNetworks.setAdapter(expListViewAdapterConfiguredNetworks);
+
+
         wiFiScanNetworksButton = (Button) findViewById(R.id.btnScanNetworks);
         wiFiScanNetworksButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 wiFiScanNetworksButton.setEnabled(false);
                 startScan();
+//                LinearLayout ll = (LinearLayout) findViewById(R.id.linearLayoutScanNetworks);
+//                LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) ll.getLayoutParams();
+//                lp.weight = lp.weight == 1 ? 0.5f : (lp.weight == 0.5f ? 2 : 1);
+//                ll.setLayoutParams(lp);
             }
         });
+
+        final LinearLayout llScanNetworks = (LinearLayout) findViewById(R.id.linearLayoutScanNetworks);
+        final LinearLayout llConfNetworks = (LinearLayout) findViewById(R.id.linearLayoutConfiguredNetworks);
+        final LinearLayout llConsole = (LinearLayout) findViewById(R.id.linearLinearConsole);
+
+        TextView textViewScannedNetworks = (TextView) findViewById(R.id.textViewScannedNetworks);
+        textViewScannedNetworks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleLinearLayoutWeight(llScanNetworks);
+            }
+        });
+
+        TextView textViewConfigureNetworks = (TextView) findViewById(R.id.textViewConfiguredNetworks);
+        textViewConfigureNetworks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleLinearLayoutWeight(llConfNetworks);
+            }
+        });
+
+        TextView textViewConsole = (TextView) findViewById(R.id.textViewConsole);
+        textViewConsole.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleLinearLayoutWeight(llConsole);
+            }
+        });
+    }
+
+    private void toggleLinearLayoutWeight(LinearLayout ll) {
+        LinearLayout.LayoutParams lp = (LinearLayout.LayoutParams) ll.getLayoutParams();
+        lp.weight = lp.weight == 1 ? 2 : (lp.weight == 2 ? 3 : 1);
+        ll.setLayoutParams(lp);
     }
 
     // create a broadcast receives to receive wifi scan notifications
     BroadcastReceiver scanReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if (WifiManager.SCAN_RESULTS_AVAILABLE_ACTION.equals(intent.getAction())){
+            if (WifiManager.SCAN_RESULTS_AVAILABLE_ACTION.equals(intent.getAction())) {
                 scanComplete();
             }
         }
@@ -85,7 +130,7 @@ public class WiFiControlActivity extends Activity {
         }
         wiFiScanNetworksButton.setEnabled(true);
         tvConsole.append("\n   ");
-       stopScan();
+        stopScan();
     }
 
     private void stopScan() {
@@ -94,11 +139,8 @@ public class WiFiControlActivity extends Activity {
 
     private void startScan() {
         registerReceiver(scanReceiver, intentFilter);
-        ((WifiManager)getSystemService(Context.WIFI_SERVICE)).startScan();
+        ((WifiManager) getSystemService(Context.WIFI_SERVICE)).startScan();
     }
-
-
-
 
 
 }
