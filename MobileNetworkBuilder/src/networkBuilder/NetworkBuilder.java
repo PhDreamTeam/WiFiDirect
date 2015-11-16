6894f711871092b0e729d965b97470a3271aa3c3
ateofilo <ateofilo@gmail.com>
2015-11-16 12:02:53 +0000

    With MobileNetworkBuilder - v0.072 - now check bug in scenario CL3 - buuuu22

--- MobileNetworkBuilder/src/networkBuilder/NetworkBuilder.java
+++ MobileNetworkBuilder/src/networkBuilder/NetworkBuilder.java
@@ -33,7 +33,7 @@ import java.util.List;
  *  - cliente: um novo GO, que não está ligado a nada, mas o CL vê que esse GO vê outro
  *  GO activo e com espaço e portanto não se liga
  *
- *
+ * buu2
  */
 public class NetworkBuilder extends JFrame {
 
